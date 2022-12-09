7649baa99c76fff1b41ef3785d953851f8fab703
Aidan Mark-Davidson <amarkdavidson@student.neumont.edu>
2022-12-09 08:13:13 -0700

    RayTracer Final

--- Renderer/Renderer.cpp
+++ Renderer/Renderer.cpp
@@ -1,7 +1,42 @@
 #include "Renderer.h"
-#include "../Object/Scene.h"
 #include "../Object/Object.h"
+#include "../Object/Scene.h"
+#include "Camera.h"
 #include <iostream>
+
+void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera, int samples)
+{
+	for (int y = 0; y < canvas.GetHeight(); y++)
+	{
+		for (int x = 0; x < canvas.GetWidth(); x++)
+		{
+
+			color3 color{ 0, 0, 0 };
+			for (int s = 0; s < samples; s++)
+			{
+
+				// get normalized (0 - 1) u, v coordinates from screen x and y 
+				float u = x / (float)canvas.GetWidth();
+				float v = 1 - (y / (float)canvas.GetHeight());
+				// add random value (0-1) to screen x and y for anti-aliasing  
+				glm::vec2 point = glm::vec2{ random01() + x, random01() + y } / glm::vec2{ canvas.m_width, canvas.m_height };
+
+				// flip y 
+				point.y = 1.0f - point.y;
+
+				// create ray from camera 
+				Ray ray = camera.PointToRay(point);
+
+				// cast ray into scene 
+				RaycastHit raycastHit;
+				// add trace color value to color 
+				color += scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
+			}
+			color /= samples;
+			canvas.DrawPoint({ x, y }, color4(color, 1));
+		}
+	}
+}
 
 bool Renderer::Initialize()
 {
@@ -10,62 +45,28 @@ bool Renderer::Initialize()
 		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
-
 	return true;
 }
-
 void Renderer::Shutdown()
 {
 	if (m_window) SDL_DestroyWindow(m_window);
 	if (m_renderer) SDL_DestroyRenderer(m_renderer);
-
 	SDL_Quit();
 }
-
-void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera)
-{
-	// camera / viewport 
-	glm::vec3 lowerLeft{ -2, -1, -1 };
-	glm::vec3 eye{ 0, 0, 0 };
-	glm::vec3 right{ 4, 0, 0 };
-	glm::vec3 up{ 0, 2, 0 };
-
-	for (int y = 0; y < canvas.GetHeight(); y++)
-	{
-		for (int x = 0; x < canvas.GetWidth(); x++)
-		{
-			glm::vec2 point = glm::vec2{ x, y } / glm::vec2{ canvas.m_width, canvas.m_height };
-			// flip y 
-			point.y = 1.0f - point.y;
-
-			// create ray from camera 
-			Ray ray = camera.PointToRay(point);
-
-			// cast ray into scene, get color 
-			RaycastHit raycastHit;
-			color3 color = scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
-			canvas.DrawPoint({ x, y }, color4(color, 1));
-		}
-	}
-}
-
-//bool Renderer::CreateWindow(std::string& name, int width, int height)
-//{
-//	return false;
-//}
-
 bool Renderer::CreateWindow(int width, int height)
 {
-	m_window = SDL_CreateWindow("Ray tracing", 100, 100, width, height, SDL_WINDOW_SHOWN); if (m_window == nullptr)
+	m_window = SDL_CreateWindow("Ray Tracer", 100, 100, width, height, SDL_WINDOW_SHOWN);
+	if (m_window == nullptr)
 	{
-		std::cout << "SDLError:" << SDL_GetError() << std::endl;
+		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return false;
 	}
-	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	m_renderer = SDL_CreateRenderer(m_window, -
+		1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (m_renderer == nullptr)
 	{
-		std::cout << "SDLError:" << SDL_GetError() << std::endl;
+		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
 	return true;
